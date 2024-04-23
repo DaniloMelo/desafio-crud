94bc898e4f6b82a9b0b40af6020eebc858261336
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-04-23 20:46:30 -0300

    fix: getAllUsers service

--- src/services/getAllusers.js
+++ src/services/getAllusers.js
@@ -1,18 +1,5 @@
 import execQuery from "../controllers/execQuery.js"
 
-// export default async function getAllusers() {
-//   return new Promise((resolve, reject) => {
-//     const data =  execQuery("SELECT * FROM users")
-
-//     if (data.error) {
-//       reject(data)
-//     }
-
-//     resolve(data.results)
-//   })
-// }
-
-
 export default async function getAllusers() {
   try {
     const data = await execQuery("SELECT * FROM users")
